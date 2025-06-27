cd3bf4933feb3e1be4f9d3188d44cc020cb986cb
Alfred Lau <alfred.lau@efunity.tech>
2025-06-27 09:43:56 +0800

    rdp setup

---
+++ rdp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e  # Exit on any error
+
+sudo apt install xrdp -y
+sudo apt install xfce4 xfce4-session -y
+echo "xfce4-session" > ~/.xsession
+sudo apt install ubuntu-desktop -y
+ 
+sudo systemctl enable --now xrdp
+ 
+sudo systemctl restart xrdp
+sudo ufw allow 3389/tcp
+
+echo "✅ RDP set up"
